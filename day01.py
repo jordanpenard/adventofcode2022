2b15b015daeda1f94a7bc64d8beb4c219a924c1d
Jordan Penard <jordan038@gmail.com>
2022-12-21 10:41:25 +0000

    Added part 2 to day 1

--- day01.py
+++ day01.py
@@ -5,5 +5,9 @@ string_per_elf = f.read().split("\n\n")
 list_per_elf = [i.split("\n") for i in string_per_elf]
 list_of_int = [[int(i) for i in j if i] for j in list_per_elf]
 cal_per_elf = list(map(sum, list_of_int))
+sorted_list_of_cal = sorted(cal_per_elf, reverse=True)
 
-print(sorted(cal_per_elf, reverse=True)[0])
+print("Part 1 : " + str(sorted_list_of_cal[0]))
+
+top_3_sum = sum(sorted_list_of_cal[0:3])
+print("Part 2 : " + str(top_3_sum))
